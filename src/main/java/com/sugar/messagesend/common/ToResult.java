b19506c99a6f13a19632e54ff1c86a2a4b9bd3b3
xingly-cn <xiaoheikeji@vip.qq.com>
2021-08-14 16:33:09 +0800

    统一返回类型封装

---
+++ src/main/java/com/sugar/messagesend/common/ToResult.java
@@ -0,0 +1,20 @@
+package com.sugar.messagesend.common;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * @Description：通用返回类型封装
+ * @Auther： 一枚方糖
+ * @Date： /08/14/16:31/
+ */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ToResult {
+    private String code;
+    private String msg;
+    private Integer count;
+    private Object data;
+}
