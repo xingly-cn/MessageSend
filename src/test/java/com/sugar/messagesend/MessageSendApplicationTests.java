c958ddec5a22195830d84dfc049f99d6509727ec
xingly-cn <xiaoheikeji@vip.qq.com>
2021-08-14 16:29:50 +0800

    SendMessage

---
+++ src/test/java/com/sugar/messagesend/MessageSendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sugar.messagesend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MessageSendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
