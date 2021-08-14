f7510efd9911a289ea4bd1c8d97431b3e5bcd2cb
xingly-cn <xiaoheikeji@vip.qq.com>
2021-08-14 18:57:07 +0800

    短信配置

---
+++ src/main/java/com/sugar/messagesend/common/PhoneConfig.java
@@ -0,0 +1,17 @@
+package com.sugar.messagesend.common;
+
+import lombok.Data;
+import lombok.Getter;
+
+/**
+ * @Description：短信发送配置
+ * @Auther： 一枚方糖
+ * @Date： /08/14/18:19/
+ */
+
+public class PhoneConfig {
+     public static String SID = "369a84b59a8ee7a37496d593acde27a7";
+     public static String TOKEN = "71e604dabb4de001b1247bd428401b1e";
+     public static String APPID = "635a26eeb204472593e3b0e83dc80b7c";
+     public static String TEMPLATED = "576788";
+}
