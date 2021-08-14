8cf74b57b8a0df34a48c6213540f3cdee92f5a69
xingly-cn <xiaoheikeji@vip.qq.com>
2021-08-14 16:29:15 +0800

    短信发送实体类

---
+++ src/main/java/com/sugar/messagesend/entity/PhoneInfo.java
@@ -0,0 +1,23 @@
+package com.sugar.messagesend.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+
+/**
+ * @Description：短信实体类
+ * @Auther： 一枚方糖
+ * @Date： /08/14/16:20/
+ */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class PhoneInfo {
+    private Integer id;
+    private String username;
+    private String phone;
+    private String content;
+    private Date sendTime;
+}
