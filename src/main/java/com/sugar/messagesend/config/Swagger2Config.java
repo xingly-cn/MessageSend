70de97acb2471995976d1ee7fb423b8fda1c2171
xingly-cn <xiaoheikeji@vip.qq.com>
2021-08-14 19:31:28 +0800

    引入knife4j

---
+++ src/main/java/com/sugar/messagesend/config/Swagger2Config.java
@@ -0,0 +1,16 @@
+package com.sugar.messagesend.config;
+
+import com.github.xiaoymin.knife4j.spring.annotations.EnableKnife4j;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+/**
+ * @Description：启用knife4j
+ * @Auther： 一枚方糖
+ * @Date： /08/14/19:30/
+ */
+@Configuration
+@EnableSwagger2
+@EnableKnife4j
+public class Swagger2Config {
+}
