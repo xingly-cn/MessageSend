ac31773d580b9c5eeae710b86d572859d5b0217b
xingly-cn <xiaoheikeji@vip.qq.com>
2021-08-14 16:28:59 +0800

    短信发送控制器

---
+++ src/main/java/com/sugar/messagesend/controller/SendByPhoneController.java
@@ -0,0 +1,13 @@
+package com.sugar.messagesend.controller;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * @Description：短信发送控制器
+ * @Auther： 一枚方糖
+ * @Date： /08/14/16:19/
+ */
+@Controller
+public class SendByPhoneController {
+
+}
