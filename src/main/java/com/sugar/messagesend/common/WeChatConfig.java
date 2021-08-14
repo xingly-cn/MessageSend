eeee9238394847868080687d34eb662e8786f4d0
xingly-cn <xiaoheikeji@vip.qq.com>
2021-08-14 21:17:08 +0800

    微信推送配置

---
+++ src/main/java/com/sugar/messagesend/common/WeChatConfig.java
@@ -0,0 +1,10 @@
+package com.sugar.messagesend.common;
+
+/**
+ * @Description：微信推送配置
+ * @Auther： 一枚方糖
+ * @Date： /08/14/21:05/
+ */
+public class WeChatConfig {
+    public static String TOKEN = "510188bc052847c7adb448f45ee39750";
+}
