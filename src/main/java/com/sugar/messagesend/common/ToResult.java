91e8f904deaf4975604fcbfae51f7d0cdc5e43af
xingly-cn <xiaoheikeji@vip.qq.com>
2021-08-14 16:41:29 +0800

    修改code类型

--- src/main/java/com/sugar/messagesend/common/ToResult.java
+++ src/main/java/com/sugar/messagesend/common/ToResult.java
@@ -13,7 +13,7 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class ToResult {
-    private String code;
+    private Integer code;
     private String msg;
     private Integer count;
     private Object data;
